24b36fe41da2a7b50300a9ab4b5e9a4df5c07c3d
Shubham Tandale <shubhamtandale@outlook.in>
2022-09-19 11:12:00 +0530

    added elementary os image to the bcrypt poc script

--- bcrypt-poc-docker.sh
+++ bcrypt-poc-docker.sh
@@ -4,7 +4,7 @@ function runDockerCommand() {
   echo "done $1" 
 }
 declare -a fileNames=('bcrypt-poc-10' 'bcrypt-poc-12' 'bcrypt-poc-14' 'bcrypt-poc-16' 'bcrypt-poc-18')
-declare -a images=('ubuntu' 'fedora' 'archlinux' 'centos')
+declare -a images=('ubuntu' 'fedora' 'archlinux' 'centos' 'elementary/docker')
 for i in ${images[@]}; do
   for j in ${fileNames[@]}; do
     runDockerCommand "docker run -it -v ~/TSP/Node/bcrypt-poc:/data $i ./data/$j"
