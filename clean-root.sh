d5a904ba3be5b7eb30344e3dabea87d8b95638e5
Shubham Tandale <shubhamtandale@outlook.in>
2023-04-27 13:31:06 +0530

    - added clean root script to clean data at root partition - added remove wine script

---
+++ clean-root.sh
@@ -0,0 +1,11 @@
+sudo apt-get autoremove
+sudo apt-get clean 
+sudo rm -rf /tmp/*
+sudo rm -rf /var/log/*
+sudo rm -rf /var/cache/apt/archives/*
+sudo rm -rf /var/tmp/*
+sudo rm -rf /var/lib/apt/lists/*
+sudo rm -rf /var/crash/*
+sudo rm -rf /var/spool/mail/*
+sudo rm -rf /var/backups/*
+sudo rm -rf /var/lib/snapd/cache/*
---
+++ remove-wine.sh
@@ -0,0 +1,13 @@
+sudo apt-get remove wine
+
+rm -rf $HOME/.wine
+
+rm -f $HOME/.config/menus/applications-merged/wine*
+
+rm -rf $HOME/.local/share/applications/wine
+
+rm -f $HOME/.local/share/desktop-directories/wine*
+
+rm -f $HOME/.local/share/icons/????_*.xpm
+
+
